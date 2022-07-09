e02ad30d8d2bcd4a5d98d39a07b2a1f6d0846aa1
Patrick Kabuga <kabugap02@gmail.com>
2022-07-10 00:05:03 +0300

    Finished the backend programming of additional helper functions

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from random import random
 
-from helpers import apology, login_required, usd, cipher, decipher, randomNumberGenerator
+from helpers import apology, login_required, usd, cipher, decipher, randomNumberGenerator, listToString, decryptionKey
 
 # Configure application
 app = Flask(__name__)
--- helpers.py
+++ helpers.py
@@ -35,8 +35,14 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-# This script enciphers a string of text using a Caesar cipher with the given number.
 def encipher(text):
+    """
+    It takes a string, and returns a list of two strings, the first being the encrypted string, and the
+    second being the decryption key.
+    
+    :param text: The text to be encrypted
+    :return: A list of the encrypted text and the decryption key.
+    """
     values = []
     newText = ""
     key = randomNumberGenerator()
@@ -52,17 +58,29 @@ def encipher(text):
             tmp += ord("a")
         else:
             tmp += key
-        newText += chr(tmp)
-
+        char = chr(tmp)
+        newText += char
     values.append(newText)
-    values.append(key)
+    values.append(decryptionKey(key))
     return values
 
 
-# This function takes in a cipher text and a decryption key,
-#  and returns the decrypted text. It does this by
-#  looping through each character in the cipher text and shifting it by the decryption key.
 def decipher(cipherText, d_key):
+    """
+    The function takes the ciphertext and the decryption key as parameters. It then uses the fourth
+    character in the decryption key to determine the decryption key. It then uses list comprehension to
+    iterate through the ciphertext and decrypt it.
+    
+    :param cipherText: The text that is to be decrypted
+    :param d_key: The decryption key
+    :return: The decrypted text.
+    """
+    
+    # Getting the fourth character in the decryption key and subtracting 90 from it.
+    for i in range(len(d_key)):
+        if i == 3:
+            d_key = (ord(d_key[i]) - d_num)
+    
     # Using list comprehension and join
     newText = ""
     for character in cipherText:
@@ -85,18 +103,66 @@ def decipher(cipherText, d_key):
 
     return newText
 
-# This function generates a random number between -32 and 36, inclusive.
-#  If the number is even, it is negated.
 def randomNumberGenerator():
+    """
+    It generates a random number between -32 and 36, and if the number is even, it is negated.
+    :return: A random number between -32 and 36 that is odd.
+    """
     num = 0
 
-    while num < -32 or num > 36 or num == 0:
-        num = randint(-100, 100)
+    while num == 0:
+        num = randint(-32, 36)
         if num % 2 == 0:
             num = -num
 
     return num
 
+def decryptionKey(key):
+    """
+    It generates 10 random numbers, adds 90 to them, converts them to characters, and then inserts the
+    key into the fourth character
+    
+    :param key: The key that the user inputs
+    :return: A list of 10 characters
+    """
+    SIZE = 8
+    random_numbers = []
+    random_characters = []
+    random = randomNumberGenerator()
+
+    # Getting 10 random numbers and adding 90 to them for later character conversion
+    for i in range(SIZE):
+        copy = random
+        flag = 1
+        # Ensuring a number is not the same as the previous
+        while flag == True:
+            if copy != random:
+                tmp = random + d_num
+                random_numbers.append(tmp)
+                flag = 0
+            else:
+                random = randomNumberGenerator()
+
+    # Converting the 10 random numbers to characters
+    for i in range(SIZE):
+        if i == 3:
+            # Reassigning the fourth character as the key
+            key += d_num
+            random_characters.append(chr(key))
+            continue
+        char = chr(random_numbers[i])
+        random_characters.insert(i, char)
+
+    return random_characters
+
+# Function to convert a list to a string
+def listToString(s):
+   
+    # initialize an empty string
+    str1 = ""
+    
+    # return string 
+    return (str1.join(s))
 
 def usd(value):
     """Format value as USD."""
