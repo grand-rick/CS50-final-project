fdc87056c93f8819329f327df68827f2e77dbcfe
Patrick Kabuga <kabugap02@gmail.com>
2022-07-12 23:51:15 +0300

    Added a js file to enable copying to clipboard

--- static/index.js
+++ static/index.js
@@ -1,45 +1,37 @@
 function copyEncryptedData() {
-    /* Get the text field */
-    const copyText = document.getElementById('encData');
-  
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-     /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
-  
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
-  }
+    let text = document.getElementById("encData").value;
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        alert('Text copied to clipboard');
+    })
+    .catch(err => {
+        alert('Error in copying text: ', err);
+    });
+}
 
 
 function copyDecryptionKey() {
     /* Get the text field */
-    const copyText = document.getElementById('d_key');
+    let text = document.getElementById('d_key').value;
 
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-        /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
-
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        alert('Text copied to clipboard');
+    })
+    .catch(err => {
+        alert('Error in copying text: ', err);
+    });
 }
 
 function copyDecryptedData() {
     /* Get the text field */
-    const copyText = document.getElementById('decryptedData');
+    let text = document.getElementById('decryptedData').value;
     
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-    
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
-    
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        alert('Text copied to clipboard');
+    })
+    .catch(err => {
+        alert('Error in copying text: ', err);
+    });
 }
