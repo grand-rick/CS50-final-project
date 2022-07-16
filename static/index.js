0bc4bdc092cbb8c7245848a6e9b38a07e5383978
Patrick Kabuga <kabugap02@gmail.com>
2022-07-16 04:02:44 +0300

    Finalized the project

--- static/index.js
+++ static/index.js
@@ -35,3 +35,26 @@ function copyDecryptedData() {
         alert('Error in copying text: ', err);
     });
 }
+
+
+/* Creating a function that will create an alert message. */
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+
+const alertUser = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alertUser('If in the encrypted data you encounter the symbol "", please refresh the page to re-encrypt!', 'danger')
+  })
+}
